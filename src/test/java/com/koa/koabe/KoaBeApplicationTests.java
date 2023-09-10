3f63b73fd9ad5ad085717caf55993d98c6cdc8a2
안정후 <austinan@naver.com>
2023-09-10 13:43:25 +0900

    feat: init project

---
+++ src/test/java/com/koa/koabe/KoaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koa.koabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
