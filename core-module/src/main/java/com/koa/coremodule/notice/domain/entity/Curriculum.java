da897d95007a6c8fea254a5c69d1aea198a3150c
seeunjang <wisjang1@naver.com>
2023-09-14 12:33:36 +0900

    feat : Curriculum Entity 생성

---
+++ core-module/src/main/java/com/koa/coremodule/notice/domain/entity/Curriculum.java
@@ -0,0 +1,24 @@
+package com.koa.coremodule.notice.domain.entity;
+
+import com.koa.commonmodule.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity
+public class Curriculum extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "curriculum_id")
+    private Long id;
+
+    private String curriculumName;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "notice_id")
+    private Notice notice;
+}
