c1d6632303bcb3e2a3505811f700c379f159b036
seeunjang <wisjang1@naver.com>
2023-09-14 12:43:51 +0900

    Vote,VoteItem Entity  생성

---
+++ core-module/src/main/java/com/koa/coremodule/vote/domain/entity/VoteItem.java
@@ -0,0 +1,21 @@
+package com.koa.coremodule.vote.domain.entity;
+
+import com.koa.commonmodule.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity
+public class VoteItem extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "vote_item_id")
+    private Long id;
+
+    private String voteItemName;
+    private Integer count;
+}
