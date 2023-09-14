8acb0e57ee1a8b70c09a01f279511fca7da267ac
seeunjang <wisjang1@naver.com>
2023-09-14 11:57:45 +0900

    feat : User, UserInfo 생성

---
+++ core-module/src/main/java/com/koa/coremodule/user/domain/entity/User.java
@@ -0,0 +1,31 @@
+package com.koa.coremodule.user.domain.entity;
+
+import com.koa.commonmodule.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String name;
+    private String email;
+    private String profileImageUrl;
+    private String userId;
+    private String password;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+}
---
+++ core-module/src/main/java/com/koa/coremodule/user/domain/entity/UserInfo.java
@@ -0,0 +1,21 @@
+package com.koa.coremodule.user.domain.entity;
+
+import com.koa.commonmodule.domain.BaseEntity;
+import jakarta.persistence.*;
+
+public class UserInfo extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_info_id")
+    private Long id;
+
+    private String major;
+    private String part;
+    private String phoneNumber;
+    private String description;
+
+    
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
