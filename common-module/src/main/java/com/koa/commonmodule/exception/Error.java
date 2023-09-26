b864e50fe9b3f97ddd769a0c3dbbdb223e51e409
seeunjang <wisjang1@naver.com>
2023-09-26 22:29:03 +0900

    feat : common-module에 BusinessException 추가 및 전역 예외 처리기 추가

---
+++ common-module/src/main/java/com/koa/commonmodule/exception/Error.java
@@ -0,0 +1,24 @@
+package com.koa.commonmodule.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum Error {
+    // Security
+    INVALID_TOKEN("유효하지 않은 토큰입니다.", 1001),
+    EXPIRED_TOKEN("만료된 토큰입니다.", 1002),
+    NOT_EXIST_TOKEN("토큰이 존재하지 않습니다.", 1003),
+    INVALID_AUTHORIZATION_TYPE("유효하지 않은 Authorization Type 입니다.", 1004),
+    EMPTY_AUTHORIZATION_HEADER("Authorization Header가 비어있습니다.", 1005),
+    AUTH_FAIL("로그인에 실패했습니다.", 1006),
+
+    // MEMBER
+    MEMBER_NOT_FOUND("사용자를 찾을 수 없습니다.", 2000);
+    private final String message;
+    private final int errorCode;
+
+    Error(String message, int errorCode) {
+        this.message = message;
+        this.errorCode = errorCode;
+    }
+}
