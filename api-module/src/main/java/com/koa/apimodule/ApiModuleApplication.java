d632c7f7492558c7f0602c21e39dfdff34c93af9
seeunjang <wisjang1@naver.com>
2023-09-28 12:13:57 +0900

    ComponentScan 추가, UserDetailsServiceAutoConfiguration.class 설정되지 않게 설정

--- api-module/src/main/java/com/koa/apimodule/ApiModuleApplication.java
+++ api-module/src/main/java/com/koa/apimodule/ApiModuleApplication.java
@@ -2,8 +2,11 @@ package com.koa.apimodule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class })
+@ComponentScan(basePackages = "com.koa")
 public class ApiModuleApplication {
 
     public static void main(String[] args) {
