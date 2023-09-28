de92630e29b176c4de3e2fd64b1383501e2a125c
seeunjang <wisjang1@naver.com>
2023-09-28 12:46:16 +0900

    feat : Token을 통해 email 추출하는 domain service, Token 저장하는 domain service 추가

---
+++ core-module/src/main/java/com/koa/coremodule/auth/domain/repository/TokenRepository.java
@@ -0,0 +1,11 @@
+package com.koa.coremodule.auth.domain.repository;
+
+import com.koa.coremodule.auth.domain.entity.Token;
+import com.koa.coremodule.auth.domain.entity.TokenType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<Token, Long> {
+    Optional<Token> findByTokenValueAndTokenType(String email, TokenType tokenType);;
+}
---
+++ core-module/src/main/java/com/koa/coremodule/auth/domain/service/TokenSaveService.java
@@ -0,0 +1,16 @@
+package com.koa.coremodule.auth.domain.service;
+
+import com.koa.commonmodule.annotation.DomainService;
+import com.koa.coremodule.auth.domain.entity.Token;
+import com.koa.coremodule.auth.domain.entity.TokenType;
+import com.koa.coremodule.auth.domain.repository.TokenRepository;
+import lombok.RequiredArgsConstructor;
+
+@DomainService
+@RequiredArgsConstructor
+public class TokenSaveService {
+    private final TokenRepository tokenRepository;
+    public void saveToken( final String token, final String email, final TokenType tokenType){
+        tokenRepository.save(Token.createToken(tokenType, email, token));
+    }
+}
