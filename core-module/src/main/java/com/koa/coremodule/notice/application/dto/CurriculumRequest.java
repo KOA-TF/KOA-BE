977c26bb46141033e6de8430240c28c465b7733a
안정후 <austinan@naver.com>
2023-10-09 21:07:49 +0900

    feat: curriculum dtos

---
+++ core-module/src/main/java/com/koa/coremodule/notice/application/dto/CurriculumRequest.java
@@ -0,0 +1,4 @@
+package com.koa.coremodule.notice.application.dto;
+
+public record CurriculumRequest(Long id) {
+}
---
+++ core-module/src/main/java/com/koa/coremodule/notice/application/dto/CurriculumResponse.java
@@ -0,0 +1,7 @@
+package com.koa.coremodule.notice.application.dto;
+
+public record CurriculumResponse(Long curriculumId,
+                                 String curriculum,
+                                 String name,
+                                 String title) {
+}
