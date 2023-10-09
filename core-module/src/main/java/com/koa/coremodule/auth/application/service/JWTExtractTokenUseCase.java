a67b500a689d4d6cfdb0e0640cd2f871f79f71db
seeunjang <wisjang1@naver.com>
2023-10-09 21:58:00 +0900

    refactor : JWTExtractTokenUseCase TokenExtractUtils 사용하도록 수정

--- core-module/src/main/java/com/koa/coremodule/auth/application/service/JWTExtractTokenUseCase.java
+++ core-module/src/main/java/com/koa/coremodule/auth/application/service/JWTExtractTokenUseCase.java
@@ -4,6 +4,7 @@ import com.koa.commonmodule.annotation.ApplicationService;
 import com.koa.commonmodule.exception.Error;
 import com.koa.coremodule.auth.application.common.consts.AuthConsts;
 import com.koa.coremodule.auth.application.exception.InvalidAuthorizationTypeException;
+import com.koa.coremodule.auth.application.utils.TokenExtractUtils;
 import org.springframework.util.StringUtils;
 
 import java.util.Objects;
@@ -11,16 +12,6 @@ import java.util.Objects;
 @ApplicationService
 public class JWTExtractTokenUseCase {
     public String extractToken(final String tokenHeader) {
-        if(!StringUtils.hasText(tokenHeader)){
-            throw new InvalidAuthorizationTypeException(Error.EMPTY_AUTHORIZATION_HEADER);
-        }
-
-        final String[] splitToken = tokenHeader.split(" ");
-        final String authorizationType = splitToken[0];
-        final String accessToken = splitToken[1];
-        if(!Objects.equals(authorizationType, AuthConsts.AUTHENTICATION_TYPE)){
-            throw new InvalidAuthorizationTypeException(Error.INVALID_AUTHORIZATION_TYPE);
-        }
-        return accessToken;
+        return TokenExtractUtils.extractToken(tokenHeader);
     }
 }
