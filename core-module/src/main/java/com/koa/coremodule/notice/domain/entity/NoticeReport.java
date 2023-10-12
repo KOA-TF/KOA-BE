b7b6919346f80bced63a96592372c18abde5d95a
안정후 <austinan@naver.com>
2023-10-12 11:35:42 +0900

    fix: notice report entity

--- core-module/src/main/java/com/koa/coremodule/notice/domain/entity/NoticeReport.java
+++ core-module/src/main/java/com/koa/coremodule/notice/domain/entity/NoticeReport.java
@@ -1,6 +1,7 @@
 package com.koa.coremodule.notice.domain.entity;
 
 import com.koa.coremodule.comment.domain.entity.Comment;
+import com.koa.coremodule.member.domain.entity.Member;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -16,7 +17,11 @@ public class NoticeReport {
     @Column(name = "notice_report_id")
     private Long id;
 
-    private Integer view;
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "comment_id")
