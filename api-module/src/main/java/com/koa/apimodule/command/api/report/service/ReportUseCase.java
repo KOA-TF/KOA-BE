dc1a9cce235cabf2c84795dd9c86eac807e6b176
안정후 <austinan@naver.com>
2023-10-13 09:40:28 +0900

    refactor: unused import & change name

--- api-module/src/main/java/com/koa/apimodule/command/api/report/service/ReportUseCase.java
+++ api-module/src/main/java/com/koa/apimodule/command/api/report/service/ReportUseCase.java
@@ -4,7 +4,7 @@ import com.koa.commonmodule.exception.Error;
 import com.koa.coremodule.notice.domain.entity.NoticeReport;
 import com.koa.coremodule.report.application.dto.ReportRequest;
 import com.koa.coremodule.report.application.exception.ReportException;
-import com.koa.coremodule.report.application.service.ReportFacadeService;
+import com.koa.coremodule.report.application.service.ReportQueryService;
 import com.koa.coremodule.report.mapper.ReportMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,18 +17,18 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ReportUseCase {
 
-    private final ReportFacadeService reportFacadeService;
+    private final ReportQueryService reportQueryService;
     private final ReportMapper reportMapper;
 
-    public Long createReport (ReportRequest request) {
+    public Long createReport(ReportRequest request) {
 
-        Optional<NoticeReport> existingReport = reportFacadeService.findById(request.getCommentId());
+        Optional<NoticeReport> existingReport = reportQueryService.findById(request.getCommentId());
 
         if (existingReport.isPresent()) {
             throw new ReportException(Error.DUPLICATE_REPORT);
         } else {
             NoticeReport reportEntity = reportMapper.toReportEntity(request);
-            NoticeReport savedReport = reportFacadeService.save(reportEntity);
+            NoticeReport savedReport = reportQueryService.save(reportEntity);
             return savedReport.getId();
         }
     }
--- core-module/src/main/java/com/koa/coremodule/report/application/service/ReportQueryService.java
+++ core-module/src/main/java/com/koa/coremodule/report/application/service/ReportQueryService.java
@@ -1,11 +1,6 @@
 package com.koa.coremodule.report.application.service;
 
-import com.koa.commonmodule.exception.BusinessException;
-import com.koa.commonmodule.exception.Error;
 import com.koa.coremodule.notice.domain.entity.NoticeReport;
-import com.koa.coremodule.report.application.dto.ReportRequest;
-import com.koa.coremodule.report.application.exception.ReportException;
-import com.koa.coremodule.report.mapper.ReportMapper;
 import com.koa.coremodule.report.repository.ReportRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,7 +9,7 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class ReportFacadeService {
+public class ReportQueryService {
 
     private final ReportRepository reportRepository;
 
