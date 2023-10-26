fd455dd43c06ed7da008edab3a262a288d738881
seeunjang <wisjang1@naver.com>
2023-10-26 18:30:54 +0900

    refactor : 사용자 정보 조회 Response값에 상세 정보 작성 유무 필드 추가

--- core-module/src/main/java/com/koa/coremodule/member/application/dto/response/MemberInfoResponse.java
+++ core-module/src/main/java/com/koa/coremodule/member/application/dto/response/MemberInfoResponse.java
@@ -1,4 +1,4 @@
-package com.koa.coremodule.member.application.dto;
+package com.koa.coremodule.member.application.dto.response;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -8,7 +8,8 @@ import lombok.Getter;
 @Getter
 @Builder
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
-public class MemberInfo {
+public class MemberInfoResponse {
     private String name;
     private String email;
+    private boolean isMemberDetailExist;
 }
