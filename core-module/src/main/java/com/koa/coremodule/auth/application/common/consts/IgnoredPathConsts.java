c46ca02aa9c222faf19d829f72a202e902d92d5d
seeunjang <wisjang1@naver.com>
2023-11-04 19:41:07 +0900

    feat : 이메일 인증 관련 API Token 검증 무시 처리

--- core-module/src/main/java/com/koa/coremodule/auth/application/common/consts/IgnoredPathConsts.java
+++ core-module/src/main/java/com/koa/coremodule/auth/application/common/consts/IgnoredPathConsts.java
@@ -1,5 +1,6 @@
 package com.koa.coremodule.auth.application.common.consts;
 
+import java.util.LinkedHashMap;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,17 +13,24 @@ import java.util.Set;
 public class IgnoredPathConsts {
 
     @Getter
-    private static Map<String, Set<HttpMethod>> ignoredPath = Map.of(
-            "/v1/auth/reissue", Set.of(HttpMethod.GET),
-            "/v1/member/register", Set.of(HttpMethod.POST),
-            "/h2-console/**", Set.of(HttpMethod.GET, HttpMethod.POST),
-            "/v1/auth/login/**", Set.of(HttpMethod.GET, HttpMethod.POST),
-            "/api-docs/**", Set.of(HttpMethod.GET, HttpMethod.POST),
-            "/error", Set.of(HttpMethod.GET, HttpMethod.POST),
-            "/favicon.ico", Set.of(HttpMethod.GET, HttpMethod.POST),
-            "/swagger-ui/**", Set.of(HttpMethod.GET, HttpMethod.POST),
-            "/swagger-resources/**", Set.of(HttpMethod.GET, HttpMethod.POST),
-            "/v3/api-docs/**", Set.of(HttpMethod.GET, HttpMethod.POST)
-    );
+    private static Map<String, Set<HttpMethod>> ignoredPath = createIgnoredPathMap();
 
+    private static Map<String, Set<HttpMethod>> createIgnoredPathMap() {
+        Map<String, Set<HttpMethod>> map = new LinkedHashMap<>();
+
+        map.put("/v1/auth/reissue", Set.of(HttpMethod.GET));
+        map.put("/v1/member/register", Set.of(HttpMethod.POST));
+        map.put("/h2-console/**", Set.of(HttpMethod.GET, HttpMethod.POST));
+        map.put("/v1/auth/login/**", Set.of(HttpMethod.GET, HttpMethod.POST));
+        map.put("/v1/member/verify", Set.of(HttpMethod.POST));
+        map.put("/v1/member/verify/code", Set.of(HttpMethod.POST));
+        map.put("/api-docs/**", Set.of(HttpMethod.GET, HttpMethod.POST));
+        map.put("/error", Set.of(HttpMethod.GET, HttpMethod.POST));
+        map.put("/favicon.ico", Set.of(HttpMethod.GET, HttpMethod.POST));
+        map.put("/swagger-ui/**", Set.of(HttpMethod.GET, HttpMethod.POST));
+        map.put("/swagger-resources/**", Set.of(HttpMethod.GET, HttpMethod.POST));
+        map.put("/v3/api-docs/**", Set.of(HttpMethod.GET, HttpMethod.POST));
+
+        return map;
+    }
 }
