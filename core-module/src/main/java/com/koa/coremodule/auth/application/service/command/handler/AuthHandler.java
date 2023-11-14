cf07ce327637074482b62fdeee26b8dc8fafaad7
seeunjang <wisjang1@naver.com>
2023-11-14 22:36:54 +0900

    refactor : 커맨드 패턴 제거

--- core-module/src/main/java/com/koa/coremodule/auth/application/service/command/handler/AuthHandler.java
+++
@@ -1,9 +0,0 @@
-package com.koa.coremodule.auth.application.service.command.handler;
-
-import com.koa.coremodule.auth.application.dto.AuthRequest;
-
-public interface AuthHandler {
-    String handle(AuthRequest authenticationInfo);
-
-    boolean isAccessible(AuthRequest authenticationInfo);
-}
