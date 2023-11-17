07f6865b7a6205de93bd37a26013da43cd48e3ff
안정후 <austinan@naver.com>
2023-11-17 10:08:40 +0900

    feat: sql settings

--- core-module/src/main/resources/V1__init.sql
+++ core-module/src/main/resources/V1__init.sql
@@ -2,21 +2,13 @@ CREATE TABLE member
 (
     member_id         BIGINT AUTO_INCREMENT PRIMARY KEY,
     name              VARCHAR(255),
+    "period"          VARCHAR(255),
     email             VARCHAR(255),
-    profile_image_url VARCHAR(255),
+    phone_number      VARCHAR(255),
     password          VARCHAR(255),
-    authority         VARCHAR(50)
-);
-
-CREATE TABLE memberinfo
-(
-    member_info_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    major          VARCHAR(255),
-    part           VARCHAR(255),
-    phone_number   VARCHAR(20),
-    description    TEXT,
-    member_id      BIGINT,
-    FOREIGN KEY (member_id) REFERENCES member (member_id)
+    fcm_token         VARCHAR(255),
+    authority         VARCHAR(50),
+    is_deleted        BOOLEAN
 );
 
 CREATE TABLE curriculum
@@ -29,27 +21,39 @@ CREATE TABLE curriculum
 
 CREATE TABLE notice
 (
-    notice_id     BIGINT AUTO_INCREMENT PRIMARY KEY,
-    title         VARCHAR(255),
-    content       TEXT,
-    member_id     BIGINT,
-    curriculum_id BIGINT,
+    notice_id        BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title            VARCHAR(255),
+    content          TEXT,
+    is_deleted       BOOLEAN,
+    member_id        BIGINT,
+    curriculum_id    BIGINT,
+    notice_team_id   BIGINT,
+    notice_image_id  BIGINT,
+    notice_view_id   BIGINT,
     FOREIGN KEY (member_id) REFERENCES member (member_id),
-    FOREIGN KEY (curriculum_id) REFERENCES curriculum (curriculum_id)
+    FOREIGN KEY (curriculum_id) REFERENCES curriculum (curriculum_id),
+    FOREIGN KEY (notice_team_id) REFERENCES  notice_team (notice_team_id),
+    FOREIGN KEY (notice_image_id) REFERENCES  notice_image (notice_image_id),
+    FOREIGN KEY (notice_view_id) REFERENCES notice_view (notice_view_id)
+);
+
+CREATE TABLE notice_image
+(
+    notice_image_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    image_url       VARCHAR(255),
+    is_deleted      BOOLEAN
 );
 
 CREATE TABLE notice_team
 (
     notice_team_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    team_name      VARCHAR(255),
-    notice_id      BIGINT,
-    FOREIGN KEY (notice_id) REFERENCES notice (notice_id)
+    team_name      VARCHAR(255)
 );
 
 CREATE TABLE notice_view
 (
     notice_view_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    view_count     INT,
+    view           VARCHAR(255),
     member_id      BIGINT,
     notice_id      BIGINT,
     FOREIGN KEY (member_id) REFERENCES member (member_id),
