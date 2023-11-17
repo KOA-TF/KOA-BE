a3f4114543ed8e6496115c29e7cb2ddef3b4d890
안정후 <austinan@naver.com>
2023-11-17 17:01:33 +0900

    test: for url

--- slack/src/main/java/com/koa/slack/config/SlackConfig.java
+++ slack/src/main/java/com/koa/slack/config/SlackConfig.java
@@ -11,7 +11,7 @@ public class SlackConfig {
 
     @Bean
     public SlackConfigProperties logConfigProperties(
-            @Value("https://hooks.slack.com/services/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") String serverWebhookUrl
+            @Value("https://hooks.slack.com/services/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") String serverWebhookUrl
     ) {
         SlackConfigProperties slackConfigProperties = new SlackConfigProperties();
         slackConfigProperties.setServerWebhookUrl(serverWebhookUrl);
