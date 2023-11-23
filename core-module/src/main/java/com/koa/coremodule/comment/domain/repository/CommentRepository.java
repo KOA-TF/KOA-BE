61297cd383aadc6a1e8da0db80384b68e676fa99
seeunjang <wisjang1@naver.com>
2023-11-23 15:16:51 +0900

    refactor : n + 1 문제 수정

--- core-module/src/main/java/com/koa/coremodule/comment/domain/repository/CommentRepository.java
+++ core-module/src/main/java/com/koa/coremodule/comment/domain/repository/CommentRepository.java
@@ -10,10 +10,10 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
 
-	@Query("select c from Comment c where c.notice.id = :noticeId and c.parent is null order by c.createdAt asc")
+	@Query("select c from Comment c join fetch c.writer where c.notice.id = :noticeId and c.parent is null order by c.createdAt asc")
 	List<Comment> findByNoticeId(@Param("noticeId") Long noticeId);
 
-	@Query("select c from Comment c where c.notice.id = :noticeId and c.parent is not null order by c.createdAt asc")
+	@Query("select c from Comment c join fetch c.writer where c.notice.id = :noticeId and c.parent is not null order by c.createdAt asc")
 	List<Comment> findChildByNoticeId(@Param("noticeId") Long noticeId);
 
 	@Query("select c from Comment c where c.parent.id = :commentId order by c.createdAt asc")
