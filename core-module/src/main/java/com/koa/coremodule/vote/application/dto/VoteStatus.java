173b50f535067ef7462a751b8345b940a8657b98
안정후 <austinan@naver.com>
2023-11-30 17:33:21 +0900

    feat: exception files & dto

---
+++ core-module/src/main/java/com/koa/coremodule/vote/application/dto/VoteStatus.java
@@ -0,0 +1,40 @@
+package com.koa.coremodule.vote.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.jackson.Jacksonized;
+
+import java.util.List;
+
+@Data
+@Builder
+@Jacksonized
+@AllArgsConstructor
+@NoArgsConstructor
+public class VoteStatus {
+
+    private Long voteId;
+    private String title;
+    private List<VoteItemStatus> items;
+
+    @Data
+    @Builder
+    @Jacksonized
+    public static class VoteItemStatus {
+        private Long voteItemId;
+        private String item;
+        private Integer count;
+        private List<MemberList> members;
+    }
+
+    @Data
+    @Builder
+    @Jacksonized
+    public static class MemberList {
+        private Long memberId;
+        private String name;
+    }
+
+}
