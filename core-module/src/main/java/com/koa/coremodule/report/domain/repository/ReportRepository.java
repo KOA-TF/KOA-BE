5ce959056c10e734d972754952569720ebed396f
안정후 <austinan@naver.com>
2024-01-22 10:54:54 +0900

    feat: report hided query service

--- core-module/src/main/java/com/koa/coremodule/report/domain/repository/ReportRepository.java
+++ core-module/src/main/java/com/koa/coremodule/report/domain/repository/ReportRepository.java
@@ -3,7 +3,11 @@ package com.koa.coremodule.report.domain.repository;
 import com.koa.coremodule.report.domain.entity.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ReportRepository extends JpaRepository<Report, Long> {
 
     Report findByCommentIdAndMemberId(Long memberId, Long commentId);
+
+    Optional<Report> findById(Long reportId);
 }
