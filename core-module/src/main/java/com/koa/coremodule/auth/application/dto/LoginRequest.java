06782844d30490b32504e2a16e88909a8fc7f878
seeunjang <wisjang1@naver.com>
2024-02-06 13:11:01 +0900

    refactor : 로그인 API RequestDto 추가, LogoutUseCase 수정 반영

---
+++ core-module/src/main/java/com/koa/coremodule/auth/application/dto/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.koa.coremodule.auth.application.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LoginRequest {
+    private String email;
+    private String password;
+}
--- core-module/src/main/java/com/koa/coremodule/auth/application/service/LogoutUseCase.java
+++ core-module/src/main/java/com/koa/coremodule/auth/application/service/LogoutUseCase.java
@@ -1,23 +1,33 @@
 package com.koa.coremodule.auth.application.service;
 
 import com.koa.commonmodule.annotation.ApplicationService;
+import com.koa.coremodule.auth.application.handler.event.AlarmTokenDeleteEvent;
 import com.koa.coremodule.auth.application.utils.TokenExtractUtils;
 import com.koa.coremodule.auth.domain.entity.Token;
 import com.koa.coremodule.auth.domain.entity.TokenType;
 import com.koa.coremodule.auth.domain.service.TokenDeleteService;
 import com.koa.coremodule.auth.domain.service.TokenQueryService;
+import com.koa.coremodule.member.domain.entity.Member;
+import com.koa.coremodule.member.domain.utils.MemberUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.transaction.annotation.Transactional;
 
 @ApplicationService
 @RequiredArgsConstructor
+@Transactional
 public class LogoutUseCase {
     private final TokenDeleteService tokenDeleteService;
     private final TokenQueryService tokenQueryService;
+    private final ApplicationEventPublisher applicationEventPublisher;
+    private final MemberUtils memberUtils;
 
     public void logoutAccessUser(String refreshTokenHeader) {
+        final Member member = memberUtils.getAccessMember();
         final String refreshToken = TokenExtractUtils.extractToken(refreshTokenHeader);
         final Token refreshTokenEntity = tokenQueryService.findTokenByTokenValue(refreshToken, TokenType.REFRESH_TOKEN);
         tokenDeleteService.deleteToken(refreshTokenEntity);
+        applicationEventPublisher.publishEvent(new AlarmTokenDeleteEvent(member.getId()));
     }
 
 }
