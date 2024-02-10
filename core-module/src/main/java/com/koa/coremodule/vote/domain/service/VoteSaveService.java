7c2479d8c0a975c17b726dc05e278edcaf00cebc
안정후 <austinan@naver.com>
2024-02-10 11:41:43 +0900

    feat: 투표 마감 처리

--- core-module/src/main/java/com/koa/coremodule/vote/domain/service/VoteSaveService.java
+++ core-module/src/main/java/com/koa/coremodule/vote/domain/service/VoteSaveService.java
@@ -1,8 +1,10 @@
 package com.koa.coremodule.vote.domain.service;
 
+import com.koa.commonmodule.exception.Error;
 import com.koa.coremodule.vote.domain.entity.Vote;
 import com.koa.coremodule.vote.domain.entity.VoteItem;
 import com.koa.coremodule.vote.domain.entity.VoteItemRecord;
+import com.koa.coremodule.vote.domain.exception.VoteException;
 import com.koa.coremodule.vote.domain.repository.VoteItemRepository;
 import com.koa.coremodule.vote.domain.repository.VoteRecordRepository;
 import com.koa.coremodule.vote.domain.repository.VoteRepository;
@@ -29,6 +31,12 @@ public class VoteSaveService {
 
     public VoteItemRecord saveVoteRecord(VoteItemRecord voteItemRecord) {
         return voteRecordRepository.save(voteItemRecord);
+    }
+
+    public void finishVote(Long voteId) {
+
+        Vote vote = voteRepository.findById(voteId).orElseThrow(() -> new VoteException(Error.VOTE_NOT_FOUND));
+        vote.updateStatus();
     }
 
 }
