c0de1636fd4b22351f5563934a5c1db358de2ebe
seeunjang <wisjang1@naver.com>
2024-02-11 01:39:47 +0900

    refactor : CommentError ErrorCode 수정, CurriculumError 추가

--- core-module/src/main/java/com/koa/coremodule/comment/domain/exception/CommentError.java
+++ core-module/src/main/java/com/koa/coremodule/comment/domain/exception/CommentError.java
@@ -8,8 +8,8 @@ import com.koa.commonmodule.exception.Error;
 @Getter
 @RequiredArgsConstructor
 public enum CommentError implements Error {
-    COMMENT_NOT_FOUND("댓글을 찾을 수 없습니다.", 4000, HttpStatus.NOT_FOUND),
-    NOT_SAME_USER("작성자가 아닙니다.", 4001, HttpStatus.BAD_REQUEST);
+    COMMENT_NOT_FOUND("댓글을 찾을 수 없습니다.", 3000, HttpStatus.NOT_FOUND),
+    NOT_SAME_USER("작성자가 아닙니다.", 3001, HttpStatus.BAD_REQUEST);
 
     private final String message;
     private final int errorCode;
